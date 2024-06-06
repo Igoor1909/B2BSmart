75e5dc89f33ec2f9f50b79aa289107fc9d129a9f
Igor Gomes <igorgomes0903@gmail.com>
2024-06-06 01:02:41 -0300

    ajuste final

--- src/main/java/com/example/B2BSmart/Controller/ClienteController.java
+++ src/main/java/com/example/B2BSmart/Controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("B2B/cliente")
+@CrossOrigin
 public class ClienteController {
 
     @Autowired
